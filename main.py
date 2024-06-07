816e15d87b2a5d6684f16c01d7bd875dc8dcacfa
CracQuiz <130645690+CracQuiz@users.noreply.github.com>
2024-06-07 10:48:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if st.button('Đăng nhập'):
     if user_name == '21521323' and pass_word == '21521323':
         st.success('Đăng nhập thành công')
         time.sleep(3)
-        wb.open('https://www.uit.edu.vn/')
+        wb.open('http://www.uit.edu.vn/')
     else:
         st.error('Sai username hoặc password')
